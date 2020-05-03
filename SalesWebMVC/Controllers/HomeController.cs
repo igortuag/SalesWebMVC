d932a27cf7129a7c291f6bfee6c152a85449f450
Igor Tuag <igortuag@gmail.com>
2020-05-03 15:45:44 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Professor"] = "Nelio Alves";
+            ViewData["Student"] = "Igor Tuag";
 
             return View();
         }
