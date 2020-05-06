d42c865169cdb0b5e68de54b1940e294c8cba8ac
Igor Tuag <igortuag@gmail.com>
2020-05-06 19:11:27 -0300

    Seeding Service

--- SalesWebMVC/Models/Enums/SaleStatus.cs
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -7,7 +7,7 @@ namespace SalesWebMVC.Models.Enums
 {
     public enum SaleStatus : int
     {
-        Peding = 0,
+        Pending = 0,
         Billed = 1,
         Canceled = 2
     }
--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,6 +9,7 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
@@ -18,10 +19,11 @@ namespace SalesWebMVC.Models
         {
         }
 
-        public Seller(int id, string name, DateTime birthDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
+            Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
